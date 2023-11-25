8f6f47b6e6a22fe618c1f7784cf5007b76429683
Agivqy Nadif <agivqynadif@gmail.com>
2023-11-25 08:40:24 +0700

    fix: fix dependencies and homepage

--- frontend/app/components/Slider.jsx
+++ frontend/app/components/Slider.jsx
@@ -1,47 +1,41 @@
-"use client";
-import React from 'react'
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+'use client';
+import React from 'react';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { Zoom } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import 'react-slideshow-image/dist/styles.css';
 
 const Slider = () => {
-    const images = [
-        "images/jumbotron-1.png",
-        "images/jumbotron-2.png",
-        "images/jumbotron-3.png",
-        "images/jumbotron-4.png",
-        "images/jumbotron-5.png",
-    ];
+  const images = ['images/jumbotron-1.png', 'images/jumbotron-2.png', 'images/jumbotron-3.png', 'images/jumbotron-4.png', 'images/jumbotron-5.png'];
 
-    const zoomInProperties = {
-        scale: 1,
-        duration: 5000,
-        transationDuration: 300,
-        infinity: true,
+  const zoomInProperties = {
+    scale: 1,
+    duration: 5000,
+    transationDuration: 300,
+    infinity: true,
 
-        backArrow: (
-            <div className='ml-10 top-40 md:top:72'>  
-                <IoIosArrowBack className='h-8 w-8 text-light cursor-pointer'/>
-            </div>
-        ),
-        forwardArrow: (
-            <div className='ml-10 top-40 md:top:72'>  
-                <IoIosArrowForward className='h-8 w-8 text-light cursor-pointer'/>
-            </div>
-        ),
-    }
+    backArrow: (
+      <div className='ml-10 top-40 md:top:72'>
+        <IoIosArrowBack className='h-8 w-8 text-light cursor-pointer' />
+      </div>
+    ),
+    forwardArrow: (
+      <div className='ml-10 top-40 md:top:72'>
+        <IoIosArrowForward className='h-8 w-8 text-light cursor-pointer' />
+      </div>
+    ),
+  };
 
-    return (
-        <div className='w-full hidden-0 relative'>
-            <Zoom {...zoomInProperties}>
-                {images.map((each,index) =>(
-                    <div key={index} className='flex justify-center items-start w-screen relative'>
-                        <img className='w-screen' src={each}/>
-                    </div>
-                ))}
-            </Zoom>
-        </div>
-    )
-}
+  return (
+    <div className='w-full hidden-0 relative'>
+      <Zoom {...zoomInProperties}>
+        {images.map((each, index) => (
+          <div key={index} className='flex justify-center items-start w-screen relative'>
+            <img className='w-screen' src={each} />
+          </div>
+        ))}
+      </Zoom>
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navigation from './components/Navigation.jsx'
+import { Inter } from 'next/font/google';
+import './styles/globals.css';
+import Navigation from './components/Navigation.jsx';
 // import NavigationBar from './components/NavigationBar'
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'PureCompute',
@@ -11,11 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        
         <Navigation />
-       
+
         {children}
       </body>
     </html>
