74e1b56c483411d207b0a22822d6e1fc9b0562e4
Ahmad Irfan Musyaffa <112853095+Irfannn13@users.noreply.github.com>
2023-11-26 22:13:00 +0700

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main>
       <Slider />
-      <BannerPromotion />
+      <BannerPromotion/>
       <WhyUs />
       <Category />
       <Brands />
