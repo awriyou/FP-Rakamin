ff3826f8d45a49cacbd946a2b4cf015483af8166
Agivqy Nadif <agivqynadif@gmail.com>
2023-11-30 15:20:05 +0700

    feat: edit back button on product detail

--- frontend/app/components/BackButton.jsx
+++ frontend/app/components/BackButton.jsx
@@ -8,14 +8,10 @@ const BackButton = () => {
   };
 
   return (
-    <button
-      className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-      onClick={handleBack}
-    >
-      <svg className='w-[15px] h-[15px] text-gray-800 dark:text-white' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 8 14'>
-        <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13' />
+    <button className='bg-transparent' onClick={handleBack}>
+      <svg className='w-[20px] h-[20px] text-cyan-700' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 14 10'>
+        <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M13 5H1m0 0 4 4M1 5l4-4' />
       </svg>
-      Back
     </button>
   );
 };
