0e36af388acac0bea0ea62f1224779cada7fa752
bagasrizq <bagas.rizqsokya@gmail.com>
2023-12-01 01:39:32 +0700

    fix: Navigation, Brands, Footer

--- frontend/app/components/Brands.jsx
+++ frontend/app/components/Brands.jsx
@@ -2,26 +2,29 @@ import React from "react";
 
 const Brands = () => {
   const category = [
-    "images/asus.png",
-    "images/lenovo.png",
-    "images/apple.png",
-    "images/acer.png",
-    "images/hp.png",
+    { src: "images/asus.png", width: "180px" },
+    { src: "images/lenovo.png", width: "180px" },
+    { src: "images/apple.png", width: "65px", height: "80px" }, // Sesuaikan tinggi dan lebar
+    { src: "images/acer.png", width: "180px" },
+    { src: "images/hp.png", width: "80px", height: "80px" }, // Sesuaikan tinggi dan lebar
   ];
 
   return (
-    <div className="hidden sm:block ">
-      <div className="container my-[80px]">
-        <div className="flex justify-center items-center gap-3 mx-[50px]">
-          {category.map((each, index) => (
-            <div key={index} className="drop-shadow-2xl relative">
-              <img className="mx-8" src={each} />
-            </div>
-          ))}
-        </div>
+    <div className="flex justify-center items-center">
+      <div className="flex gap-16">
+        {category.map((item, index) => (
+          <div key={index} className="flex items-center justify-center">
+            <img
+              className="mx-8"
+              src={item.src}
+              alt={`Brand ${index + 1}`}
+              style={{ width: item.width, height: item.height }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
-export default Brands
+export default Brands;
--- frontend/app/components/Footer.jsx
+++ frontend/app/components/Footer.jsx
@@ -1,29 +1,45 @@
 // components/Footer.jsx
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-opacity-90 bg-black text-white h-96 pt-12 pb-2">
+    <footer className="bg-opacity-90 bg-black text-white pt-12 pb-2">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <h2 className="text-lg font-bold mb-4">NAVIGATION</h2>
-            <ul>
-              <li><a href="/">HOME</a></li>
-              <li><a href="/product">PRODUCT</a></li>
-              <li><a href="/category">CATEGORY</a></li>
+            <h2 className="font-bold text-sm/[8pt] mb-4">NAVIGATION</h2>
+            <ul className="text-sm/[2px]">
+              <li className="py-3">
+                <a href="/">HOME</a>
+              </li>
+              <li className="py-3">
+                <a href="/product">PRODUCT</a>
+              </li>
+              <li className="py-3">
+                <a href="#categories">CATEGORY</a>
+              </li>
             </ul>
           </div>
-        
           <div>
-            <h2 className="text-lg font-bold mb-4">About Us</h2>
-            <p>Information about the company and the team.</p>
+            <h2 className="font-bold text-sm/[8pt] mb-4">CONTACT US</h2>
+            <ul>
+              <li className="flex align-items-center ">
+                <div className="flex items-center">
+                  <img src="images/whatsapp.png" href="https://www.whatsapp.com/" alt="" className="w-4 h-4 mr-2 cursor-pointer" />
+                  <a href="https://www.whatsapp.com/" className="text-sm/[2px]">
+                    +62812898898
+                  </a>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-        <hr className="mt-48 mb-1 border-t border-white" />
-        <p className="text-center text-sm">
-          &copy; 2023 PureCompute. All rights reserved.
-        </p>
+        <div className="block">
+          <hr className="mt-12 mb-1 border-t border-white" />
+          <p className="text-center text-sm">
+            &copy; 2023 PureCompute. All rights reserved.
+          </p>
+        </div>
       </div>
     </footer>
   );
