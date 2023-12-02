7ab22696cb2e7ed24310db38d37c3c3d7514cd2b
bagasrizq <bagas.rizqsokya@gmail.com>
2023-12-03 02:02:33 +0700

    fix: footer and favicon

--- frontend/app/(login)/[userId]/page.js
+++ frontend/app/(login)/[userId]/page.js
@@ -7,10 +7,25 @@ import Brands from '@/app/components/Brands';
 import Contact from '@/app/components/Contact';
 import FeedbackForm from '@/app/components/FeedbackForm';
 import Footer from '@/app/components/Footer';
+
+const images = [
+  "images/jumbotron-1.png",
+  "images/jumbotron-2.png",
+  "images/jumbotron-3.png",
+  "images/jumbotron-4.png",
+  "images/jumbotron-5.png",
+];
+
 export default function Home() {
   return (
     <main>
-      <Slider />
+      <div>
+        <Slider autoSlide={true}>
+          {images.map((each) => (
+            <img src= {each}/>
+          ))}
+        </Slider>
+      </div>
       <BannerPromotion/>
       <WhyUs />
       <Category />
